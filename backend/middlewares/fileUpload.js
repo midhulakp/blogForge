b4a0fd856202ace10ce970cd10261d55beb2f37b
Pranav K <pranavkcse@gmail.com>
2025-02-21 17:26:37 +0530

    add dotenv configuration for cloudinary in file upload middleware

--- backend/middlewares/fileUpload.js
+++ backend/middlewares/fileUpload.js
@@ -1,6 +1,7 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
-
+import dotenv from 'dotenv';
+dotenv.config();
 
 cloudinary.config({ 
     cloud_name: process.env.API_CLOUDNAME, 
